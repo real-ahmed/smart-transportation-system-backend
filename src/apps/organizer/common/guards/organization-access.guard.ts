e801199018efb0ab3ca5ec5ab57bc7cb05447d57
Ahmed Emad <ahmedemad1242003@icloud.com>
2025-04-18 19:23:10 +0200

    fix OrganizationAccessGuard

--- src/apps/organizer/common/guards/organization-access.guard.ts
+++ src/apps/organizer/common/guards/organization-access.guard.ts
@@ -1,51 +1,62 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { OrganizationsService } from 'src/organizations/organizations.service';
 
 @Injectable()
 export class OrganizationAccessGuard implements CanActivate {
-    constructor(private readonly organizationsService: OrganizationsService) { }
+  constructor(private readonly organizationsService: OrganizationsService) {}
 
-    async canActivate(
-        context: ExecutionContext,
-    ): Promise<boolean> {
-        const request = context.switchToHttp().getRequest();
-        const user = request.user;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
 
-        // Check organization ID from multiple possible locations
-        const organizationId =
-            request.params.organizationId ||
-            request.body.organizationId ||
-            request.query.organizationId ||
-            request.body.organization ||
-            request.query.organization ||
-            request.params.organization;
+    // Destructure with default empty objects
+    const { params = {}, body = {}, query = {} } = request;
 
-        if (!organizationId) {
-            return true; // Skip check if no organization ID provided
-        }
+    // Check organization ID from multiple possible locations
+    const organizationId =
+      params.organizationId ||
+      body.organizationId ||
+      query.organizationId ||
+      body.organization ||
+      query.organization ||
+      params.organization;
 
-        const hasAccess = await this.checkUserAccessToOrganization(user, organizationId);
-        if (!hasAccess) {
-            throw new UnauthorizedException('You do not have access to this organization');
-        }
-
-        return true;
+    if (!organizationId) {
+      return true; // Skip check if no organization ID provided
     }
 
-    private async checkUserAccessToOrganization(user: any, organizationId: string): Promise<boolean> {
-        if (!user || !organizationId) {
-            return false;
-        }
-
-        const organization = await this.organizationsService.findOne({
-            _id: organizationId,
-            $or: [
-                { owner: user.id },
-                { members: user.id }
-            ]
-        });
-
-        return !!organization;
+    const hasAccess = await this.checkUserAccessToOrganization(
+      user,
+      organizationId,
+    );
+    if (!hasAccess) {
+      throw new UnauthorizedException(
+        'You do not have access to this organization',
+      );
     }
+
+    return true;
+  }
+
+  private async checkUserAccessToOrganization(
+    user: any,
+    organizationId: string,
+  ): Promise<boolean> {
+    if (!user || !organizationId) {
+      return false;
+    }
+
+    const organization = await this.organizationsService.findOne({
+      _id: organizationId,
+      $or: [{ owner: user.id }, { members: user.id }],
+    });
+
+    return !!organization;
+  }
 }
