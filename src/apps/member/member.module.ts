6e51501d9f2979750da029374c31a0f5ac143940
Ahmed Emad <ahmedemad1242003@icloud.com>
2025-04-26 18:51:20 +0300

    add onboard end point to member

--- src/apps/member/member.module.ts
+++ src/apps/member/member.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { OnboardModule } from './onboard/onboard.module';
 import { MembershipsModule } from 'src/memberships/memberships.module';
+import { on } from 'events';
+import { OnboardController } from './onboard/onboard.controller';
 
 @Module({
-  controllers: [],
+  controllers: [OnboardController],
   imports: [
     RouterModule.register([
       {
