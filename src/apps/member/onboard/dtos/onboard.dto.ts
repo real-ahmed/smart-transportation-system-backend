bd8bf98400326921a7e7dc65a721dcb4df3206d1
Ahmed Emad <ahmedemad1242003@icould.com>
2025-07-04 18:02:43 +0300

    Update OnboardDto to change validation decorators and remove unnecessary ones

--- src/apps/member/onboard/dtos/onboard.dto.ts
+++ src/apps/member/onboard/dtos/onboard.dto.ts
@@ -14,21 +14,19 @@ export class OnboardDto {
     description: 'The ID of the organization',
     example: '507f1f77bcf86cd799439011',
   })
-  @IsMongoId()
+  @IsString()
   organization: string;
 
   @ApiProperty({
     description: 'student image',
     example: '',
   })
-  @IsString()
   studentImage: string;
 
   @ApiProperty({
     description: 'Full name of the student',
     example: 'John Doe',
   })
-  @IsString()
   studentName: string;
 
   @ApiProperty({
@@ -37,36 +35,30 @@ export class OnboardDto {
     required: false,
     type: [String],
   })
-  @IsArray()
-  @IsOptional()
   studentDisabilities: string[];
 
   @ApiProperty({
     description: 'Student Social Security Number',
     example: '123-45-6789',
   })
-  @IsString()
   studentSsn: string;
 
   @ApiProperty({
     description: 'Street address',
     example: '123 Main St',
   })
-  @IsString()
   street: string;
 
   @ApiProperty({
     description: 'City name',
     example: 'New York',
   })
-  @IsString()
   city: string;
 
   @ApiProperty({
     description: 'State name',
     example: 'NY',
   })
-  @IsString()
   state: string;
 
   @ApiProperty({
@@ -80,7 +72,6 @@ export class OnboardDto {
     description: 'Postal/ZIP code',
     example: '10001',
   })
-  @IsPostalCode('US')
   postalCode: string;
 
   // @ApiProperty({
